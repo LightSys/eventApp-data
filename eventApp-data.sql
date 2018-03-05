93a847c5c88ebbc7c81635ddcd31b439dbd19f8a
LAMP Developer <devel@lamp-vm.(none)>
2018-03-05 15:35:23 -0700

    Added comments and on deletes

--- eventApp-data.sql
+++ eventApp-data.sql
@@ -1,3 +1,21 @@
+-- Recreates database with event data
+
+drop table event;
+drop table contact_page;
+drop table contacts;
+drop table schedule_items;
+drop table info_page;
+drop table info_page_sections;
+drop table housing;
+drop table prayer_partners;
+drop table attendees;
+drop table notifications;
+drop table themes;
+drop table users;
+drop table event_users;
+
+-- Contains general information about events and data needed
+-- for an event's navigation menu
 create table event (
     ID                  varchar(36) UNIQUE,
     internal_ID         int AUTO_INCREMENT, 
@@ -25,28 +43,34 @@ create table event (
     primary key (internal_ID)
 );
 
+
+-- Contains information for laying out sections of the contact page
 create table contact_page (
     ID                  int AUTO_INCREMENT,
     event_ID            int,
     section_ID          int,
     header              varchar(100),
-    content             varchar(1000) --?
+    content             text,
 
     primary key (ID),
     foreign key (event_ID) references event(internal_ID)
+        on delete cascade
 );
 
+-- Stores contact information to populate contact page
 create table contacts (
     ID                  int AUTO_INCREMENT,
     name                varchar(100),
     address             varchar(100),
-    phone               varchar(17), --?
+    phone               varchar(17),
     event_ID            int,
 
     primary key (ID),
     foreign key (event_ID) references event(internal_ID)
+        on delete cascade
 );
 
+-- Contains information to lay out a schedule
 create table schedule_items (
     ID                  int AUTO_INCREMENT,
     date                date,
@@ -59,46 +83,32 @@ create table schedule_items (
 
     primary key (ID),
     foreign key (event_ID) references event(internal_ID)
+        on delete cascade
 );
 
-create table info_page (
-    ID                  int AUTO_INCREMENT,
-    nav                 varchar(25),
-    icon                varchar(100),
-    event_ID            int,
-
-    primary key (ID),
-    foreign key (event_ID) references event(internal_ID)
-);
-
-create table info_page_sections (
-    ID                  int AUTO_INCREMENT,
-    title               varchar(50),
-    description         text,
-    info_page_ID        int,
-
-    primary key (ID),
-    foreign key (info_page_ID) references info_page(ID)
-);
-
+-- Contains information about housing arrangements
 create table housing (
     ID                  int AUTO_INCREMENT,
     host_name           varchar(100),
-    driver              varchar(100), --?
+    driver              varchar(100),
     event_ID            int,
 
     primary key (ID),
     foreign key (event_ID) references event(internal_ID)
+        on delete cascade
 );
 
+-- Contains information about the placement of people in prayer groups
 create table prayer_partners(
     group_ID            int AUTO_INCREMENT,
     event_ID            int,
 
     primary key (group_ID),
     foreign key (event_ID) references event(internal_ID)
+        on delete cascade
 );
 
+-- Contains the names of attendees and where they have been assigned
 create table attendees (
     ID                  int AUTO_INCREMENT,
     name                varchar(30),
@@ -107,9 +117,12 @@ create table attendees (
 
     primary key (ID),
     foreign key (house_ID) references housing(ID),
+        on delete set null
     foreign key (prayer_group_ID) references prayer_partners(ID)
+        on delete set null
 );
 
+-- Stores any information needed for a notification
 create table notifications (
     ID                  int AUTO_INCREMENT,
     event_ID            int,
@@ -120,17 +133,46 @@ create table notifications (
 
     primary key (ID),
     foreign key (event_ID) references event(internal_ID)
+        on delete cascade
 );
 
--- How are themes and events related?
-create table theme (
+-- Contains information about themes
+create table themes (
     ID                  int AUTO_INCREMENT,
     theme_name          varchar(50) UNIQUE,
     theme_color         varchar(7),
+    event_ID            int,
 
-    primary key (ID)
+    primary key (ID),
+    foreign key (event_ID) references event(internal_ID)
+        on delete cascade
 );
 
+-- Defines a link on the nav bar for a user-defined page
+create table info_page (
+    ID                  int AUTO_INCREMENT,
+    nav                 varchar(25),
+    icon                varchar(100),
+    event_ID            int,
+
+    primary key (ID),
+    foreign key (event_ID) references event(internal_ID)
+        on delete cascade
+);
+
+-- Contains information to lay out a user-defined page
+create table info_page_sections (
+    ID                  int AUTO_INCREMENT,
+    title               varchar(50),
+    description         text,
+    info_page_ID        int,
+
+    primary key (ID),
+    foreign key (info_page_ID) references info_page(ID)
+        on delete cascade
+);
+
+-- Stores usernames and hashed passwords
 create table users (
     ID                  int AUTO_INCREMENT,
     username            varchar(30) UNIQUE,
@@ -139,6 +181,7 @@ create table users (
     primary key (ID)
 );
 
+-- Associates users with events they have created or can access
 create table event_users (
     ID                  int AUTO_INCREMENT,
     user_ID             int,
@@ -146,5 +189,7 @@ create table event_users (
 
     primary key (ID),
     foreign key (user_ID) references users(ID),
+        on delete set null
     foreign key (event_ID) references event(internal_ID)
+        on delete cascade
 );
