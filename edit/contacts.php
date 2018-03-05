97ece5f5d0b52d27198c8557203a1d58f0f8e53c
LAMP Developer <devel@lamp-vm.(none)>
2018-03-05 15:49:49 -0700

    templated head

--- edit/contacts.php
+++ edit/contacts.php
@@ -1,12 +1,24 @@
 <html>
-	<head>
-		<link rel="stylesheet" href="../styles/styles.css" />
-	</head>
-	
+	<?php include("../templates/head.php"); ?>
 	<body>
 		<?php include("../templates/left-nav.php"); ?>
+		<style>
+			#contacts {
+				background-color: grey;
+				color: white;
+			}
+		</style>
 
-		
+		<section id="main">
+			<h1>Contacts</h1>
+			<form>
+				<div id="person1">
+				Name: <input type="text" name="name"><br>
+				Address: <input type="text" name="address"><br>
+				Phone: <input type="text" name="phone"><br>
+				<input type="submit" value="Submit">
+			</form>
+		</section>
 
 	</body>
 </html>
