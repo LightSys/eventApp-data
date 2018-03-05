7bdb88f6bbe7eb55b9c1fc3402476e80db713486
LAMP Developer <devel@lamp-vm.(none)>
2018-03-05 16:20:16 -0700

    added button styles and function for creating more fields

--- templates/head.php
+++ templates/head.php
@@ -1,4 +1,5 @@
 <head>
+    <title>LightSys Event App Data Generator</title>
     <link rel="stylesheet" href="../styles/styles.css" />
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="../scripts/scripts.js"></script>
