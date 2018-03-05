4eb50eea29bf6b6317f23c76618b1726e30cca9e
LAMP Developer <devel@lamp-vm.(none)>
2018-03-05 16:20:16 -0700

    added button styles and function for creating more fields

--- edit/contacts.php
+++ edit/contacts.php
@@ -11,14 +11,35 @@
 
 		<section id="main">
 			<h1>Contacts</h1>
-			<form>
-				<div id="person1">
-				Name: <input type="text" name="name"><br>
-				Address: <input type="text" name="address"><br>
-				Phone: <input type="text" name="phone"><br>
+			<form id="contactForm">
+
+				<div id="contactForm">
+					<!-- Name: <input type="text" name="name"><br>
+					Address: <input type="text" name="address"><br>
+					Phone: <input type="text" name="phone"><br> -->
+				</div>
+
+				<div class="btn" onclick="addContact()">+ Add Contact</div>
 				<input type="submit" value="Submit">
 			</form>
 		</section>
 
 	</body>
+
+	<script>
+		var contactCounter = 0;
+		
+		$(document).ready(function() {
+			addContact();
+		});
+
+		function addContact() {
+			var html = '<div class="contact-card">Name: <input type="text" name="name' + contactCounter + '"><br>'
+						+ 'Address: <input type="text" name="address' + contactCounter + '"><br>'
+						+ 'Phone: <input type="text" name="phone' + contactCounter + '"><br></div>'
+			addFields(html, 'contactForm');
+			contactCounter++;
+		}
+	</script>
 </html>
+
