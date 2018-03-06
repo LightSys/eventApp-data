c541ca1db6807a82359778fc1321768b06284972
Emmett Budd <ebudd@cedarville.edu>
2018-03-06 14:29:44 -0700

    changed submit button to save everywhere, fixed some css

--- edit/prayer-partners.php
+++ edit/prayer-partners.php
@@ -1,11 +1,10 @@
 <html>
-	<?php include("../templates/head.php"); ?>
 	
 	<body>
 		<?php include("../templates/left-nav.php"); ?>
 
 		<style>
-			#prayerpartners {
+			#prayer-partners {
 				background-color: grey;
 				color: white;
 			}
@@ -13,16 +12,16 @@
 
 		<section id="main">
 			<h1>Prayer Partners</h1>
-			<form id="PPartnersForm">
+			<form id="form">
 				<div id="sectionCards">
 				</div>
 				<div class="btn" onclick="addGroup()">Add Group</div>
-				<input type="submit" value="Submit">
+				<div class="btn" id="save">Save</div>
 			</form>
 		</section>
 
 	</body>
-
+	<?php include("../templates/head.php"); ?>
 	<script>
 		var counter = 0;
 		var partnerCounter = 0;
@@ -33,15 +32,15 @@
 
 		function addGroup() {
 			guestCounter = 0;
-			var html = '<div class="contact-card">Partners: <div id="partners' +
-						 counter + '"><input type="text" name="partner' + guestCounter + '"><br></div>' +						
-						 '<div class="btn" onclick="addPartner(' + counter + ')">Add Partner</div>';
+			var html = '<div class="card">Partners: <br><div id="partners' + counter + '">'
+						+ '<div class="input"><input type="text" name="partner' + guestCounter + '"></div></div>'					
+						+ '<br><div class="btn" onclick="addPartner(' + counter + ')">Add Partner</div></div>';
 			addFields(html, 'sectionCards');
 			counter++;
 		}
 
 		function addPartner(num) {
-			var html = '<input type="text" name="partner' + counter + '"><br>';
+			var html = '<div class="input"><input type="text" name="partner' + counter + '"></div>';
 			addFields(html, 'partners' + num);
 			partnerCounter++;
 		}
