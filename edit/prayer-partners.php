536150d7fd8aa7c691c203f99c700a86ab08fcc7
LAMP Developer <devel@lamp-vm.(none)>
2018-03-06 11:47:46 -0700

    changed prayerpartners.php and advanced.php

--- edit/prayer-partners.php
+++ edit/prayer-partners.php
@@ -4,7 +4,47 @@
 	<body>
 		<?php include("../templates/left-nav.php"); ?>
 
-		
+		<style>
+			#prayerpartners {
+				background-color: grey;
+				color: white;
+			}
+		</style>
+
+		<section id="main">
+			<h1>Prayer Partners</h1>
+			<form id="PPartnersForm">
+				<div id="sectionCards">
+				</div>
+				<div class="btn" onclick="addGroup()">Add Group</div>
+				<input type="submit" value="Submit">
+			</form>
+		</section>
 
 	</body>
+
+	<script>
+		var counter = 0;
+		var partnerCounter = 0;
+		
+		$(document).ready(function() {
+			addGroup();
+		});
+
+		function addGroup() {
+			guestCounter = 0;
+			var html = '<div class="contact-card">Partners: <div id="partners' +
+						 counter + '"><input type="text" name="partner' + guestCounter + '"><br></div>' +						
+						 '<div class="btn" onclick="addPartner(' + counter + ')">Add Partner</div>';
+			addFields(html, 'sectionCards');
+			counter++;
+		}
+
+		function addPartner(num) {
+			var html = '<input type="text" name="partner' + counter + '"><br>';
+			addFields(html, 'partners' + num);
+			partnerCounter++;
+		}
+	</script>
+
 </html>
