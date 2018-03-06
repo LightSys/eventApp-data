bdcdca85331c1ce322c03ec9e27e3171186b68d6
Emmett Budd <ebudd@cedarville.edu>
2018-03-06 11:20:39 -0700

    fixed guest counter

--- edit/housing.php
+++ edit/housing.php
@@ -25,26 +25,29 @@
 
 	<script>
 		var counter = 0;
-		var guestCounter = 0;
+		var guestCounters = [];
+		// var guestCounter = 0;
 		
 		$(document).ready(function() {
 			addHost();
 		});
 
 		function addHost() {
-			guestCounter = 0;
+			guestCounters[counter] = 0;
 			var html = '<div class="card"><div class="input">Host: <input type="text" name="host' + counter + '"></div>'
 						+ '<div class="input">Driver: <input type="text" name="driver' + counter + '"></div>'
-						+ '<div class="input">Guests: <div id="guests' + counter + '"><input type="text" name="guest' + guestCounter + '"></div></div>'
+						+ '<div class="input">Guests: <div id="guests' + counter + '"><input type="text" name="guest' + guestCounters[counter] + '"></div></div>'
 						+ '<div class="btn" onclick="addGuest(' + counter + ')">Add Guest</div>';
 			addFields(html, 'sectionCards');
+			console.log("addHost, " + counter);
 			counter++;
 		}
 
 		function addGuest(num) {
-			var html = '<input type="text" name="guest' + counter + '">';
+			var html = '<input type="text" name="guest' + guestCounters[num] + '">';
 			addFields(html, 'guests' + num);
-			guestCounter++;
+			console.log("addGuest " + guestCounters[num]);
+			guestCounters[num]++;
 		}
 	</script>
 </html>
