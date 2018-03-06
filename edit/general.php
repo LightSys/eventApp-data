cf6d49843b78569aef05ce42f54228b38f743136
Emmett Budd <ebudd@cedarville.edu>
2018-03-06 10:40:03 -0700

    working on CSS for forms

--- edit/general.php
+++ edit/general.php
@@ -13,10 +13,11 @@
 		<section id="main">
 			<h1>General</h1>
 			<form>
-				Event Name: <input type="text" name="name"><br>
-				Logo: <input type="file" name="logo"><br>
-				Time Zone: <input type="text" name="timezone"><br>
-				Welcome Message: <input type="text" name="welcome"><br>
+				<div class="input">Event Name:<input type="text" name="name"></div>
+				<div class="input">Logo:<input type="file" name="logo"></div>
+				<div class="input">Time Zone:<input type="text" name="timezone"></div>
+				<div class="input">Welcome Message:<input type="text" name="welcome"></div>
+				<div class="input">Visible:<input type="checkbox" name="visible" value="false" checked="unchecked"></div>
 				<input type="submit" value="Submit">
 			</form>
 		</section>
