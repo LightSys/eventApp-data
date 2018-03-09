6319522c8208a2c25d9fac49b021c6e5e0126d44
LAMP Developer <devel@lamp-vm.(none)>
2018-03-08 18:00:52 -0700

    Generated QR code

--- qr.php
+++ qr.php
@@ -1,26 +1,11 @@
-<!DOCTYPE html>
-<html>
-<body>
-
 <?php
 
-    include(phpqrcode/qrlib.php);
+    include('phpqrcode/qrlib.php');
+    include('helper.php');
 
     $event_ID = (isset($_GET['id']) ? "?id=".$_GET['id'] : "");
-    $url = (isset($_SERVER['HTTPS']) ? "https" : "http") . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
-    $token = strtok($url, '/');
-
-    $url = "";
-    while ($token !== "qr.php") {
-        $url .= $token."/";
-        $token = strtok("/");
-    }
+    $url = stripFileName();
     $url .= "getevent.php" . $event_ID;
-    echo $url;
     QRcode::png($url);
-
+    
 ?>
-
-</body>
-</html>
-
