6c89314250ff7f9f67c00c7ee13dbc5f709a315c
Emmett Budd <ebudd@cedarville.edu>
2018-03-09 12:16:03 -0700

    fixed sql errors

--- edit/general.php
+++ edit/general.php
@@ -25,11 +25,8 @@
 		die();
 	}	
 
-    if(isset($_POST['name']))
-	{
-		if (!($stmt = $db->prepare("UPDATE event SET name = :name, time_zone = :time_zone, welcome_message = :welcome_message visible = :visible, logo = :logo WHERE id = :id"))) {
-			// die(0);
-		}
+    if(isset($_POST['name'])) {
+		$stmt = $db->prepare("UPDATE event SET name = :name, time_zone = :time_zone, welcome_message = :welcome_message, visible = :visible, logo = :logo WHERE id = :id");
 		
 		$name = $_POST['name'];
 		$timeZone = $_POST['timezone'];
@@ -63,27 +60,17 @@
 			}
 		}
 		
-		if (!($stmt->bindValue(':name', $name))) {
-			// die(1);
-		}
-		if (!($stmt->bindValue(':time_zone', $timeZone))) {
-			// die(2);
-		}
-		if (!($stmt->bindValue(':welcome_message', $welcomeMessage))) {
-			// die(3);
-		}
-		if (!($stmt->bindValue(':id', $id))) {
-			// die(4);
-		}	
-		if (!($stmt->bindValue(':visible', $visible))) {
-			// die(5);
-		}	
-		if (!($stmt->bindValue(':logo', $logo))) {
-			// die(6);
-		}	
-		if(!($stmt->execute())) {
-			// die(7);
-		}
+		$stmt->bindValue(':name', $name);
+		$stmt->bindValue(':time_zone', $timeZone);
+		$stmt->bindValue(':welcome_message', $welcomeMessage);
+		$stmt->bindValue(':id', $id);
+		$stmt->bindValue(':visible', $visible);	
+		$stmt->bindValue(':logo', $logo);
+		$stmt->execute();
+
+		// reroute to this page with the new event id
+		header("Location: ".full_url($_SERVER)."?id=".$_POST['id']);
+		die();
 	}
 	
 	include("../templates/check-event-exists.php");
@@ -105,6 +92,7 @@
 			<h1>General</h1>
 				<form action = "general.php" method = "post" enctype="multipart/form-data" id="form">
 					<div class="card">
+						<input type="hidden" name="id" value="<?php echo($_GET['id'])?>">
 						<div class="input">Event Name:<input type="text" name="name"></div>
 						<div class="input">Logo:<input type="file" name="logo"></div>
 						<div class="input">Time Zone:<input type="text" name="timezone"></div>
