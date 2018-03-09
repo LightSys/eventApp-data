73bc6948fd13907986a4a3e072486f8690b92005
Emmett Budd <ebudd@cedarville.edu>
2018-03-08 17:14:16 -0700

    working on adding and updating on housing

--- edit/housing.php
+++ edit/housing.php
@@ -4,31 +4,49 @@
 
 include("../connection.php");
 
-if( isset($_POST['host'] )) {
-	foreach($_POST['host'] as $key => $host) {
-		if (!($stmt = $db->prepare("INSERT into housing(event_ID, host_name, driver) VALUES (:event_ID, :host_name, :driver)"))) {
+if( isset($_POST['action'] )) {
+
+	if($_POST['action'] == 'addHousing') {
+
+		$event_id = $_GET['id'];
+
+		$stmt = $db->prepare('INSERT into housing(event_ID, sequential_ID) values (:event_id, (SELECT IFNULL(MAX(temp.sequential_ID),0)+1 from (select sequential_ID from housing where event_ID=:event_id) as temp), "", "")');
+		$stmt->bindValue(":event_id",$event_id);
+		$stmt->execute();
+
+		$housing_id = $db->lastInsertId();
+
+		$stmt = $db->prepare('INSERT into housing(event_ID, sequential_ID) values (:event_id, :sequential_ID)');
+		$stmt->bindValue(':event_id', $event_id);
+		$stmt->bindValue(":sequential_ID",$housing_id);
+		$stmt->execute();
+
+	} else if ($_POST['action'] == 'updateHousing') {	
+
+		// die("update housing");
+
+		// UPDATE contacts set name = :name, address = :address, phone = :phone, event_ID = :event_id"
+		if (!($stmt = $db->prepare("UPDATE housing set event_ID = :event_ID, host_name = :host_name, driver = :driver where event_ID = :id and sequential_ID = :sequence"))) {
 			die(0);
 		}
 
-		$ID = $_GET['id'];
-		$driver = $_POST['driver'][$key];
+		foreach($_POST['host'] as $key => $host) {
+			$ID = $_GET['id'];
+			$driver = $_POST['driver'][$key];
 
-		if (!($stmt->bindValue(':event_ID', $ID))) {
-			die(1);
-		}
-		if (!($stmt->bindValue(':host_name', $host))) {
-			die(2);
-		}
-		if (!($stmt->bindValue(':driver', $driver))) {
-			die(3);
-		}
-
-		if(!($stmt->execute())) {
-			die();
+			echo $ID . "<br>";
+			echo $driver . "<br>";
+			echo $host . "<br>";
+			
+			$stmt->bindValue(":sequence", $key);
+			$stmt->bindValue(':event_ID', $ID);
+			$stmt->bindValue(':host_name', $host);
+			$stmt->bindValue(':driver', $driver);
+			$stmt->bindValue(":sequence", $_POST['sequence']);
+			$stmt->execute();
 		}
 	}
 }
-
  ?>
 
 <html>
@@ -47,38 +65,49 @@ if( isset($_POST['host'] )) {
 		<section id="main">
 			<h1>Housing</h1>
 			<?php echo('<form id="form" action="housing.php?id=' . $_GET['id'] . '" method="post">') ?>
-				<div id="sectionCards">
-				</div>
+				<input type="hidden" name="id" value = "<?php echo $_GET["id"]?>">
+				<input type="hidden" name="action" value = "updateHousing">
+				
+				<?php
+					$event_id = $_GET["id"];
+					$get_housing_stmt = $db->prepare("SELECT * FROM housing where event_ID=:id");
+					$get_housing_stmt->bindValue(":id",$event_id);
+					$get_housing_stmt->execute();
+
+
+					// look through query
+					while($get_housing_res = $get_housing_stmt->fetch(PDO::FETCH_ASSOC)){ 
+						echo '<div id="sectionCards"><div class="card"><div class="input">Host: <input type="text" name="host[]" value = '.$get_housing_res['host_name'].'></div>'
+						. '<div class="input">Driver: <input type="text" name="driver[]" value = ' .$get_housing_res['driver'].'></div>'
+						. '<div class="input">Guests: <div id="guests[]"><select id="contact[]"><option>contact name</option></select></div><br><br>'
+						. '<div class="btn" onclick="addGuest([])">Add Guest</div></div></div>';
+					}
+				?>
+				<br>
 				<div class="btn" onclick="addHost()">Add Host</div>
 				<div class="btn" id="save">Save</div>
 			</form>
 		</section>
+		<form id = "addHousing" action = "housing.php?id=<?php echo $_GET["id"]?>" method="post">
+			<input type = "hidden" name="sequence" value="">
+			<input type="hidden" name="id" value = "<?php echo $_GET["id"]?>">
+			<input type="hidden" name="action" value = "addHousing">
+		</form>
 
 	</body>
 
 	<?php include("../templates/head.php"); ?>
 	<script>
-
-		$(document).ready(function() {
-			addHost();
-		});
-
-
-		// FIXME: broke ability to add guests
 		function addHost() {
-			var html = '<div class="card"><div class="input">Host: <input type="text" name="host[]"></div>'
-						+ '<div class="input">Driver: <input type="text" name="driver[]"></div>'
-						+ '<div class="input">Guests: <div id="guests[]"><select id="contact[]"><option>contact name</option></select></div><br><br>'
-						+ '<div class="btn" onclick="addGuest([])">Add Guest</div></div>';
-			addFields(html, 'sectionCards');
+			// $("#addHousing > #sequence").value(sequential_ID);
+			$("#addHousing").submit();
 		}
 
 		function addGuest(num) {
-			var html = '<select id="contact[]"><option>contact name</option></select>'
+			var html = '<select id="contact[]"><option>contact name</option></select>';
 			addFields(html, 'guests' + num);
 		}
 	</script>
 
 	
 </html>
-
