b90b5727f0df1c62e12dfb4107a5b98539dd5fb5
judsonhayes <judsonhayes@letu.edu>
2018-06-11 13:47:22 -0600

    url redirect changes

--- edit/general.php
+++ edit/general.php
@@ -46,7 +46,7 @@
 		header("Location: ".full_url($_SERVER)."?id=".$id);
 		die();
 	}
-	secure();
+	secure(full_url($_SERVER));
 
         $get_event_stmt = $db->prepare("SELECT logo,name,time_zone,TZcatagory,welcome_message, visible,contact_nav,contact_icon,sched_nav,sched_icon,housing_nav,housing_icon,prayer_nav,prayer_icon,notif_nav,notif_icon FROM event WHERE ID =:id");
 	
@@ -70,22 +70,32 @@
 		$id = $_POST["id"];
 		$logo=null;
 		// If the user specified a logo file
+		
 		if(isset($_FILES["logo"]["name"])) {
+			
+
+			
 		//	die( "attempted upload");// we got this far
 			// The directory to save the file to
 			$uploaddir = '../temp/';
 			// Get the full path to save the uploaded file to
 			$uploadfile = $uploaddir . basename($_FILES['logo']['name']);
 			// Try to upload the file
-			if(move_uploaded_file($_FILES['logo']['tmp_name'], $uploadfile)) {
-				$logo = base64_encode(file_get_contents($uploadfile));
-			//	die("encoding should be successful"); failed by this point
-				echo "<p>File succesfully uploaded</p>";
-			} else {
+			$imageFileType = strtolower(pathinfo($uploadfile,PATHINFO_EXTENSION));
+			if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg" && $imageFileType != "gif" ) {
+    					die ("Sorry, only JPG, JPEG, PNG & GIF files are allowed.");
+			}
+			else{
+				if(move_uploaded_file($_FILES['logo']['tmp_name'], $uploadfile)) {
+					$logo = base64_encode(file_get_contents($uploadfile));
+				//	die("encoding should be successful"); failed by this point
+					echo "<p>File succesfully uploaded</p>";
+				} else {
 
-				die("   did not move file"); //apparently there is a permission failure
+					die("did not move file"); //apparently there is a permission failure
 
-				echo "<p>Error uploading file</p>";
+					echo "<p>Error uploading file</p>";
+				}
 			}
 		
 			// Remove the contents of the temporary directory
